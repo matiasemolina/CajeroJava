3cff3b6b27877d50232c24ed235960a0aa9046fe
JooGo01 <joseakd136@gmail.com>
2023-07-30 22:05:41 -0300

    Controller y Mapper de Cajero

---
+++ src/controlador/CajeroController.java
@@ -0,0 +1,112 @@
+package controlador;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import entities.Cajero;
+import entities.Sucursal;
+import mapper.BaseMapper;
+import mapper.CajeroMapper;
+
+public class CajeroController extends ControladorBase<Cajero>{
+	
+	public CajeroController() {
+	}
+	
+	protected List<Cajero> getByParameters(Cajero request, String query){
+		List<Cajero> response = new ArrayList<>();
+		CajeroMapper mappeador= new CajeroMapper();
+		try {
+			PreparedStatement prepareStatement = ConectorBD.getInstance().getConnection().prepareStatement(query);
+			if(request.getId()!=0) {
+				prepareStatement.setLong(1, request.getId());
+			}
+			
+			if(request.getUbicacion()!=null) {
+				prepareStatement.setString(2, request.getUbicacion());
+			}
+			
+			if(request.getSucursal()!=null) {
+				prepareStatement.setObject(3, request.getSucursal());
+			}
+			
+			ResultSet resultado = prepareStatement.executeQuery();
+			while(resultado.next()) {
+				response.add(mappeador.toEntity(resultado));
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return response;
+	}
+	
+	public List<Cajero> getById(Long id){
+		Cajero cajero= new Cajero();
+		cajero.setId(id);
+		String query ="Select * from CAJERO where CAJERO.id = ?";
+		List<Cajero> response = getByParameters(cajero,query);
+		return response;
+	}
+	
+	public List<Cajero> getByUbicacion(String ubicacion){
+		Cajero cajero=new Cajero();
+		cajero.setUbicacion(ubicacion);
+		String query ="Select * from CAJERO where CAJERO.ubicacion = ?";
+		List<Cajero> response = getByParameters(cajero,query);
+		return response;
+	}
+	
+	public List<Cajero> getByUbicacion(Sucursal sucursal){
+		Cajero cajero=new Cajero();
+		cajero.setSucursal(sucursal);
+		String query ="Select * from CAJERO where CAJERO.id_sucursal = ?";
+		List<Cajero> response = getByParameters(cajero,query);
+		return response;
+	}
+	
+	public List<Cajero> readAll(){
+		CajeroMapper mappeador = new CajeroMapper();
+		List<Cajero> cajero=new ArrayList<Cajero>();
+		cajero=readAll("SELECT * FROM CAJERO", mappeador);
+		return cajero;
+	}
+	
+	@Override
+	protected List readAll(String consulta, BaseMapper mapper) {
+		List cajero = new ArrayList<>();
+		try {
+			Statement statement = ConectorBD.getInstance().getConnection().createStatement();
+			ResultSet resultado = statement.executeQuery(consulta);
+			while(resultado.next()) {
+				cajero.add(mapper.toEntity(resultado));
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return cajero;
+	}
+	
+	public Integer insert (Cajero request) {
+		String query="INSERT INTO CAJERO(id, ubicacion, id_sucursal) VALUES (?,?,?)";
+		return insert(request, query);
+	}
+	
+	@Override
+	protected Integer insert(Cajero cajero, String sql) {
+		Integer response=0;
+		try {
+			PreparedStatement prepareStatement=ConectorBD.getInstance().getConnection().prepareStatement(sql);
+			prepareStatement.setLong(1, cajero.getId());
+			prepareStatement.setString(2, cajero.getUbicacion());
+			prepareStatement.setObject(3, cajero.getSucursal());
+			response = prepareStatement.executeUpdate();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return response;
+	}
+	
+}
--- src/mapper/CajeroMapper.java
+++ src/mapper/CajeroMapper.java
@@ -4,6 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import controlador.ConectorBD;
 import entities.Cajero;
 public class CajeroMapper implements BaseMapper<Cajero>{
 
@@ -13,6 +14,9 @@ public class CajeroMapper implements BaseMapper<Cajero>{
 		Cajero resultado = new Cajero();
 		try {
 			//faltan los .set
+			resultado.setId(resultSet.getLong("id"));
+			resultado.setUbicacion(resultSet.getString("ubicacion"));
+			resultado.setSucursal(resultSet.getLong("id_sucursal"));
 		}catch(SQLException e) {
 			e.printStackTrace();
 		}
@@ -24,6 +28,11 @@ public class CajeroMapper implements BaseMapper<Cajero>{
 		// TODO Auto-generated method stub
 		try {
 			//falta llenar el preparedstatement con los datos del sql y del controlador
+			PreparedStatement prepareStatement=ConectorBD.getInstance().getConnection().prepareStatement(sql);
+			prepareStatement.setLong(1, t.getId());
+			prepareStatement.setString(2, t.getUbicacion());
+			prepareStatement.setObject(3, t.getSucursal());
+			return prepareStatement;
 		}catch(SQLException e) {
 			e.printStackTrace();
 		}
